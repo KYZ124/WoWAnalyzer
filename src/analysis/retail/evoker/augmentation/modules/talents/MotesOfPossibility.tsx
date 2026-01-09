acd1306d0be747e12a731aa92b120916d4ca8989
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2026-01-09 14:14:01 +0000

    Add role-based tracking for Motes of Possibility

--- src/analysis/retail/evoker/augmentation/modules/talents/MotesOfPossibility.tsx
+++ src/analysis/retail/evoker/augmentation/modules/talents/MotesOfPossibility.tsx
@@ -16,15 +16,31 @@ import Events, { ApplyBuffEvent, HasRelatedEvent, RefreshBuffEvent } from 'parse
 import { VersatilityIcon } from 'interface/icons';
 import DonutChart from 'parser/ui/DonutChart';
 import { SpellLink } from 'interface';
+import Combatants from 'parser/shared/modules/Combatants';
+import ROLES from 'game/ROLES';
+import SPECS, { Spec } from 'game/SPECS';
 /**
  * Eruption has a 25% chance to create a Mote of Possibility. Motes of Possibility can be consumed to grant a player Shifting Sands, Inferno's Blessing, or Symbiotic Bloom at random.
  * Clairvoyant: Chance increased to 35%, and can instead grant Prescience.
  */
 class MotesOfPossibility extends Analyzer {
+  static dependencies = {
+    combatants: Combatants,
+  };
+  protected combatants!: Combatants;
   sandsMotes = 0;
   infernoMotes = 0;
   blossomMotes = 0;
   prescienceMotes = 0;
+
+  personalMotes = 0;
+  tankMotes = 0;
+  healerMotes = 0;
+  meleeHealerMotes = 0;
+  prevokerHealerMotes = 0;
+  meleeDPSMotes = 0;
+  rangedDPSMotes = 0;
+  midrangeDPSMotes = 0;
 
   constructor(options: Options) {
     super(options);
@@ -110,31 +126,76 @@ class MotesOfPossibility extends Analyzer {
 
   OnInfernosApply(event: ApplyBuffEvent | RefreshBuffEvent) {
     this.infernoMotes += 1;
+    this.OnNonSandsMotesApply(event);
   }
 
   OnInfernosApplyWithTalent(event: ApplyBuffEvent | RefreshBuffEvent) {
     if (!HasRelatedEvent(event, FIRE_BREATH_INFERNOS_APPLY)) {
       this.infernoMotes += 1;
+      this.OnNonSandsMotesApply(event);
     }
   }
 
   OnSymbioticApply(event: ApplyBuffEvent | RefreshBuffEvent) {
     this.blossomMotes += 1;
+    this.OnNonSandsMotesApply(event);
   }
 
   OnSymbioticApplyWithTalent(event: ApplyBuffEvent | RefreshBuffEvent) {
     if (!HasRelatedEvent(event, EMERALD_BLOSSOM_SYMBIOTIC_APPLY)) {
       this.blossomMotes += 1;
+      this.OnNonSandsMotesApply(event);
     }
   }
 
   OnPrescienceApply(event: ApplyBuffEvent | RefreshBuffEvent) {
     this.prescienceMotes += 1;
+    this.OnNonSandsMotesApply(event);
   }
 
   OnPrescienceApplyWithTalent(event: ApplyBuffEvent | RefreshBuffEvent) {
     if (!HasRelatedEvent(event, PRESCIENCE_BUFF_CAST_LINK)) {
       this.prescienceMotes += 1;
+      this.OnNonSandsMotesApply(event);
+    }
+  }
+
+  OnNonSandsMotesApply(event: ApplyBuffEvent | RefreshBuffEvent) {
+    let buffTarget;
+    if (this.combatants.players[event.targetID]) {
+      buffTarget = event.targetID;
+    }
+    if (!buffTarget) {
+      return;
+    }
+    const buffedPlayer = this.combatants.players[buffTarget];
+    if (buffedPlayer?.spec?.role === ROLES.HEALER) {
+      if (buffedPlayer.spec === SPECS.PRESERVATION_EVOKER) {
+        this.prevokerHealerMotes += 1;
+      } else if (
+        buffedPlayer.spec === SPECS.HOLY_PALADIN ||
+        buffedPlayer.spec === SPECS.MISTWEAVER_MONK
+      ) {
+        this.meleeHealerMotes += 1;
+      } else {
+        this.healerMotes += 1;
+      }
+    } else if (buffedPlayer?.spec?.role === ROLES.TANK) {
+      this.tankMotes += 1;
+    } else if (buffTarget === this.owner.info.playerId) {
+      this.personalMotes += 1;
+    } else if (buffedPlayer?.spec?.role === ROLES.DPS.MELEE) {
+      this.meleeDPSMotes += 1;
+    } else if (buffedPlayer?.spec?.role === ROLES.DPS.RANGED) {
+      if (
+        buffedPlayer.spec === SPECS.DEVASTATION_EVOKER ||
+        buffedPlayer.spec === SPECS.AUGMENTATION_EVOKER ||
+        buffedPlayer.spec === SPECS.DEVOURER_DEMON_HUNTER
+      ) {
+        this.midrangeDPSMotes += 1;
+      } else {
+        this.rangedDPSMotes += 1;
+      }
     }
   }
 
@@ -215,6 +276,19 @@ class MotesOfPossibility extends Analyzer {
         <div className="pad">
           <label>Motes of Possibility breakdown</label>
           <DonutChart items={moteChart} />
+        </div>
+        <div>
+          <label>Motes by role</label>
+          <ul>
+            <li>Tank: {this.tankMotes}</li>
+            <li>Melee Healer: {this.meleeHealerMotes}</li>
+            <li>Prevoker: {this.prevokerHealerMotes}</li>
+            <li>Ranged Healer: {this.healerMotes}</li>
+            <li>Melee DPS: {this.meleeDPSMotes}</li>
+            <li>Midrange DPS: {this.midrangeDPSMotes}</li>
+            <li>Ranged DPS: {this.rangedDPSMotes}</li>
+            <li>Personal: {this.personalMotes}</li>
+          </ul>
         </div>
       </Statistic>
     );
